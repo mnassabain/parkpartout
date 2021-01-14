98f7bbfb956aa7e5f5f6479d2b9c4fc7bc7f5542
Marco Nassabain <marco.nassabain@hotmail.com>
2021-01-14 23:03:17 +0100

    ✨ Add static Fiche Screen

--- Screen/FicheScreen.js
+++ Screen/FicheScreen.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { Card, CardItem, Text } from 'native-base';
 
 class FicheScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>More informations about your parking!</Text>
+      <SafeAreaView style={styles.container}>
+        
+        <Card style={styles.itemInfoContainer}>
+          <CardItem style={styles.itemInfo}>
+            <View style={styles.itemHeader}>
+              <Text style={styles.itemName}>Parking Gambetta</Text>
+              <Text>50 places</Text>
+            </View>
+            <Text style={{marginBottom: 5}}>1 Parc de l'Etoile</Text>
+            <Text>1 km</Text>
+          </CardItem>
+        </Card>
 
-        <Button
-          title="Go back to your list result"
-          onPress={() =>
-            this.props.navigation.navigate('Liste')
-          }
-        />
-      </View>
+        <Card style={styles.mapContainer}>
+        </Card>
+
+      </SafeAreaView>
     );
   }
 }
@@ -22,9 +30,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  itemInfoContainer: {
+    marginTop: 50,
+    width: '90%',
+    shadowOpacity: 0,
+    paddingVertical: 5,
+    backgroundColor: '#f0f0f0',
+    marginBottom: 25,
+  },
+  itemInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    backgroundColor: '#f0f0f0',
+  },
+  itemHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  itemName: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  mapContainer: {
+    width: '90%',
+    height: 250
+  }
 });
 
 export default FicheScreen;
